d913a8758b3df3d038a3dc335c3e3d5d4b4a3746
SebLOURDEL <seb.lourdel8@gmail.com>
2024-11-22 00:15:23 +0100

    lightroom

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -13,28 +13,28 @@ const Featured = () => {
         <h3 class="titrePost">2022 FERRARI SF90 SPIDER</h3>
         <span class="auteur">$ 991,000</span>
       </div>
-        <img src="spider.webp"  className='img-slide-feat' alt="jejed"/>
+        <img src="spider.webp"  width="300" height="300"   className='img-slide-feat' alt="jejed"/>
         </div>
         <div className="slide">
         <div class="wrapInfoPost">
         <h3 class="titrePost">2022 FERRARI SF90 SPIDER</h3>
         <span class="auteur">$ 991,000</span>
       </div>
-        <img src="gt.webp"  className='img-slide-feat' alt="jejed"/>
+        <img src="gt.webp"  width="300" height="300"   className='img-slide-feat' alt="jejed"/>
         </div>
         <div className="slide">
         <div class="wrapInfoPost">
         <h3 class="titrePost">2022 FERRARI SF90 SPIDER</h3>
         <span class="auteur">$ 991,000</span>
       </div>
-        <img src="superfast.webp"  className='img-slide-feat' alt="jejed"/>
+        <img src="superfast.webp"  width="300" height="300"   className='img-slide-feat' alt="jejed"/>
         </div>
         <div className="slide">
         <div class="wrapInfoPost">
         <h3 class="titrePost">2022 FERRARI SF90 SPIDER</h3>
         <span class="auteur">$ 991,000</span>
       </div>
-        <img src="spider.webp"  className='img-slide-feat' alt="jejed"/>
+        <img src="spider.webp"  width="300" height="300"   className='img-slide-feat' alt="jejed"/>
         </div>
       </Carousel>
     </div>
